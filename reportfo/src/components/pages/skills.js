dbcff38242beb3ac64f8fe92ab694a827aa259ab
AshleyAlpha <mashleyalpha@gmail.com>
2024-04-23 01:00:43 -0700

    some mod

--- reportfo/src/components/pages/skills.js
+++ reportfo/src/components/pages/skills.js
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>See my skills</div>
+    <div>
+      <h1>I Am Skilled about:</h1>
+      <p>A web developer is a programmer who specializes in the development of
+          applications that run on the web. They are responsible for creating
+          websites, web applications, and other web-based solutions using
+          programming languages such as HTML, CSS, and JavaScript, as well as
+          various frameworks and libraries.</p>
+
+          <p>A web developer is a programmer who specializes in the development of
+          applications that run on the web. They are responsible for creating
+          websites, web applications, and other web-based solutions using
+          programming languages such as HTML, CSS, and JavaScript, as well as
+          various frameworks and libraries.</p>
+
+          <p>A web developer is a programmer who specializes in the development of
+          applications that run on the web. They are responsible for creating
+          websites, web applications, and other web-based solutions using
+          programming languages such as HTML, CSS, and JavaScript, as well as
+          various frameworks and libraries.</p>
+
+          <p>A web developer is a programmer who specializes in the development of
+          applications that run on the web. They are responsible for creating
+          websites, web applications, and other web-based solutions using
+          programming languages such as HTML, CSS, and JavaScript, as well as
+          various frameworks and libraries.</p>
+    </div>
   )
 }
 
